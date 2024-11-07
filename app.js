7c9adfea0fd35bb76467ec311a424e1bc25aa07e
RadhunRaj <raj244224@gmail.com>
2024-11-08 01:25:47 +0530

    signup updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.set('view engine','ejs');
 app.set("views",[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')]);
-app.set(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/',userRouter);
 
