829d4829444ea15308fd47c88ed84ed68b216599
RadhunRaj <raj244224@gmail.com>
2024-11-10 22:20:12 +0530

    google auth

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -9,4 +9,8 @@ router.post('/signup',userController.signup);
 router.post("/verify-otp",userController.verifyOtp);
 router.post("/resnd-otp",userController.resendOtp);
 
+router.get("/auth/google",passport.authenticate('google',{scope:['profile','email']}));
+router.get("/auth/google/callback",passport.authenticate('google',{failureRedirect:'/signup'}),(req,res)=>{
+    res.redirect('/');
+})
 module.exports = router;
