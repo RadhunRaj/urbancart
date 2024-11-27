481eae4d019c638989647741249138eafb465f47
RadhunRaj <raj244224@gmail.com>
2024-11-27 20:19:18 +0530

    admin dash

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -9,6 +9,38 @@ const loadLogin =  (req,res)=>{
     res.render("admin-login",{message:null})
 }
 
+const login = async (req,res)=>{
+    try {
+        const {email,password}=req.body;
+        const admin = await user.findOne({email,isAdmin:true});
+        if(admin){
+            const passwordMatch = bcrypt.compare(password,admin.password);
+            if(passwordMatch){
+                req.session.admin = true;
+                return res.redirect("/admin")
+            }else{
+                return res.redirect('/login');
+            }
+        }else{
+            return res.redirect('/login')
+        }
+    } catch (error) {
+        console.log('login error',error);
+        return res.redirect('/pageerror');
+    }
+}
+
+const loadDashboard = async (req,res)=>{
+    if(req.session.admin){
+        try {
+            res.render('dashboard');
+        } catch (error) {
+            res.redirect('/pageerror');
+        }
+    }
+}
 module.exports = {
-    loadLogin
+    loadLogin,
+    login,
+    loadDashboard,
 }
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const adminController = require("../controllers/admin/adminController");
 
 router.get("/login",adminController.loadLogin);
+router.post("/login",adminController.login);
+router.get("/",adminController.loadDashboard);
 
 module.exports = router;
