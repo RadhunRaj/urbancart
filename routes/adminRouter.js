1b5bd4469f3bb66a5bb93032164da1904f29f6b8
RadhunRaj <raj244224@gmail.com>
2024-11-28 22:41:12 +0530

    add brand

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const adminController = require("../controllers/admin/adminController");
 const customerController = require("../controllers/admin/customerController");
 const categoryController = require("../controllers/admin/categoryController");
+const brandController = require("../controllers/admin/brandController");
 const {userAuth,adminAuth} = require("../middlewares/auth");
+const multer = require("multer");
+const storage = require("../heplers/multer");
+const uploads = multer({storage:storage});
+
 
 router.get("/pageerror",adminController.pageerror)
+// Login & Logout
 router.get("/login",adminController.loadLogin);
 router.post("/login",adminController.login);
 router.get("/",adminAuth,adminController.loadDashboard);
@@ -13,6 +19,7 @@ router.get("/logout",adminController.logout);
 router.get("/users",adminAuth,customerController.customerInfo);
 router.get("/blockCustomer",adminAuth,customerController.customerBlocked);
 router.get("/unblockCustomer",adminAuth,customerController.customerunBlocked);
+// Category
 router.get("/category",adminAuth,categoryController.categoryInfo);
 router.post("/addCategory",adminAuth,categoryController.addCategory);
 router.post("/addCategoryOffer",adminAuth,categoryController.addCategoryOffer);
@@ -21,5 +28,7 @@ router.get("/listCategory",adminAuth,categoryController.getListCategory);
 router.get("/unListCategory",adminAuth,categoryController.getUnlistCategory);
 router.get("/editCategory",adminAuth,adminController.getEditCategory);
 router.post("/editCategory/:id",adminAuth.categoryController.editCategory);
-
+// Brand Manage
+router.get("/brands".adminAuth,brandController.getBrandPage);
+router.post("/addBrand",adminAuth,uploads.single("image"),brandController.addBrand);
 module.exports = router;
