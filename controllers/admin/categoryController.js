9ab7e874d2f93df00610e682b5e4ee5bff040732
RadhunRaj <raj244224@gmail.com>
2024-12-05 18:56:45 +0530

    cat edit

--- controllers/admin/categoryController.js
+++ controllers/admin/categoryController.js
@@ -1,18 +1,18 @@
-const category = require("../../models/categorySchema");
-const product = require("../../models/productSchema");
+const Category = require("../../models/categorySchema");
+const Product = require("../../models/productSchema");
 
 const categoryInfo = async (req,res)=>{
     try {
         const page = parseInt(req.query.page)||1;
         const limit = 4;
         const skip = (page-1)*limit;
-        const categoryData = await category.find({})
+        const categoryData = await Category.find({})
         .sort({createAt:-1})
         .skip(skip)
         .limit(limit)
 
-        const totalCategories = await category.countDocuments();
-        const totalPages = Math.ceil(totalCategories / limit);
+        const totalCategories = await Category.countDocuments();
+        const totalPages = Math.ceil(totalCategories/limit);
         res.render("category",{
             cat:categoryData,
             currentPage:page,
@@ -26,13 +26,13 @@ const categoryInfo = async (req,res)=>{
 }
 
 const addCategory = async(req,res)=>{
-    const {name,description}=req.body;
+    const {name,description} = req.body;
     try {
-        const existingCategory = await category.findOne({name});
+        const existingCategory = await Category.findOne({name});
         if(existingCategory){
-            return res.status(500).send({error:"Category already exists"})
+            return res.status(400).json({error:"Category already exists"})
         }
-        const newCategory = new category({
+        const newCategory = new Category({
             name,
             description,
         })
@@ -46,16 +46,16 @@ const addCategoryOffer = async (req,res)=>{
 try {
     const percentage = parseInt(req.body.percentage);
     const categoryId = req.body.categoryId;
-    const category = await category.findById(categoryId);
+    const category = await Category.findById(categoryId);
     if(!category){
-        return res.status(404).json({status:false,message:"category not found"});
+        return res.status(404).json({status:false , message:"category not found"});
     }
-    const products = await product.find({category:category._id});
-    const hasProductOffer = products.some((product)=>product.productOffer>percentage);
+    const products = await Product.find({category:category._id});
+    const hasProductOffer = products.some((product)=>product.productOffer > percentage);
     if(hasProductOffer){
         return res.json({status:false,message:"products within this category already have product offers"})
     }
-    await category.updateOne({_id:categoryId},{$set:{categoryOffer:percentage}});
+    await Category.updateOne({_id:categoryId},{$set:{categoryOffer:percentage}});
     for(const product of products){
         product.productOffer=0;
         product.salePrice=product.regularPrice;
@@ -69,13 +69,13 @@ try {
 const removeCategoryOffer = async(req,res)=>{
     try {
         const categoryId = req.body.categoryId;
-        const category = await category.findById(categoryId);
+        const category = await Category.findById(categoryId);
         if(!category){
             return res.status(404).json({status:false , message:"Category not found"})
         }
         const percentage = category.categoryOffer;
-        const products = await product.find({category:category._id});
-        if(products.length>0){
+        const products = await Product.find({category:category._id});
+        if(products.length > 0){
             for(const product of products){
                 product.salePrice += Math.floor(product.regularPrice * (percentage/100));
                 product.productOffer = 0;
@@ -92,7 +92,7 @@ const removeCategoryOffer = async(req,res)=>{
 const getListCategory = async(req,res)=>{
     try {
         let id = req.query.id;
-        await category.updateOne({_id:id},{$set:{isListed:false}});
+        await Category.updateOne({_id:id},{$set:{isListed:false}});
         res.redirect("/admin/category");
     } catch (error) {
         res.redirect("/pageerror");
@@ -101,7 +101,7 @@ const getListCategory = async(req,res)=>{
 const getUnlistCategory = async (req,res)=>{
     try {
         let id = req.query.id;
-        await category.updateOne({_id:id},{$set:{isListed:true}});
+        await Category.updateOne({_id:id},{$set:{isListed:true}});
         res.redirect("/admin/category")
     } catch (error) {
         res.redirect("/pageerror");
@@ -110,7 +110,7 @@ const getUnlistCategory = async (req,res)=>{
 const getEditCategory = async (req,res)=>{
     try {
         const id = req.query.id;
-        const category = await category.findOne({_id:id});
+        const category = await Category.findOne({_id:id});
         res.render("edit-category",{category:category});
     } catch (error) {
         res.redirect("/pageerror");
@@ -119,13 +119,13 @@ const getEditCategory = async (req,res)=>{
 const editCategory = async (req,res)=>{
     try {
         const id = req.params.id;
-        const {categoryName,description}=req.body;
-        const existingCategory = await category.findOne({name:categoryName});
+        const {categoryName,description} = req.body;
+        const existingCategory = await Category.findOne({name:categoryName});
         if(existingCategory){
             return res.status(400).json({error:"category exists, please select another name"})
         }
-        const upateCategory = await category.findByIdAndUpdate(id,{
-            name:categoryname,
+        const updateCategory = await Category.findByIdAndUpdate(id,{
+            name:categoryName,
             description:description,
         },{new:true});
         if(updateCategory){
--- controllers/admin/customerController.js
+++ controllers/admin/customerController.js
@@ -11,7 +11,7 @@ const customerInfo = async (req,res)=>{
             page=req.query.page
         }
         const limit = 3;
-        const data = await User.find({
+        const userData = await User.find({
             isAdmin:false,
             $or:[
                 {name:{$regex:".*"+search+".*"}},
@@ -29,9 +29,13 @@ const customerInfo = async (req,res)=>{
                 {email:{$regex:".*"+search+".*"}},
             ],
         }).countDocuments();
-        res.render("customers")
+        res.render("customers",{
+            data:userData,
+            totalPages:Math.ceil(count/limit),
+            currentPage:page
+        })
     } catch (error) {
-        
+        res.redirect("/pageerror");
     }
 }
 const customerBlocked = async (req,res)=>{
@@ -40,7 +44,7 @@ const customerBlocked = async (req,res)=>{
         await User.updateOne({_id:id},{$set:{isBlocked:true}});
         res.redirect("/admin/users");
     } catch (error) {
-        res.direct("/pageerror")
+        res.redirect("/pageerror")
     }
 }
 const customerunBlocked = async(req,res)=>{
